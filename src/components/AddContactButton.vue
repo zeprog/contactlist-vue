2fe192d3e5904f378324fdd6159d2e62866cf181
zeprog <dmitrium02@mail.ru>
2021-01-09 00:33:00 +0300

    Added a button to create a contact

---
+++ src/components/AddContactButton.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="add-button">
+    <div class="add-button__body">
+      <p>+</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AddContactButton'
+}
+</script>
+
+<style lang="scss">
+$mainColor: #00BFFF;
+$hoverColor: #20c7ff;
+
+.add-button {
+  display: fixed;
+  position: absolute;
+  top: 88%;
+  left: 94%;
+
+  .add-button__body {
+    width: 60px;
+    height: 60px;
+    background-color: $mainColor;
+    border-radius: 50%;
+    cursor: pointer;
+
+    &:hover {
+      background-color: $hoverColor;
+    }
+
+    p {
+      color: #fff;
+      text-align: center;
+      font-size: 42px;
+      line-height: 60px;
+    }
+  }
+}
+</style>
--- src/views/ContactList.vue
+++ src/views/ContactList.vue
@@ -2,18 +2,21 @@
   <div class="list">
     <Header />
     <Content />
+    <AddContactButton />
   </div>
 </template>
 
 <script>
 import Header from '@/components/Header.vue'
 import Content from '@/components/Content.vue'
+import AddContactButton from '@/components/AddContactButton.vue'
 
 export default {
   name: 'ContactList',
   components: {
     Header,
-    Content
+    Content,
+    AddContactButton
   }
 }
 </script>
